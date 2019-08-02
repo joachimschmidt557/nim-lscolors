0272962762c87d0bab9941245883170f7306c22b
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-08-02 14:37:09 +0200

    Add tests for style parsing

---
+++ tests/test_parse_styles.nim
@@ -0,0 +1,34 @@
+import unittest, options
+
+import lscolors/style
+
+test "parse empty":
+  let style = parseStyle("")
+  assert style.isNone
+
+test "parse bold":
+  let style = parseStyle("01")
+  assert style.get.fg.isNone
+  assert style.get.bg.isNone
+  assert style.get.font == FontStyle(bold: true, italic: false, underline: false)
+
+test "parse yellow":
+  let style = parseStyle("33")
+  assert style.get.fg.get.kind == ck8
+  assert style.get.fg.get.ck8Val == c8Yellow
+  assert style.get.bg.isNone
+  assert style.get.font == defaultFontStyle()
+
+test "parse some fixed color":
+  let style = parseStyle("38;5;220;1")
+  assert style.get.fg.get.kind == ckFixed
+  assert style.get.fg.get.ckFixedVal == 220u8
+  assert style.get.bg.isNone
+  assert style.get.font == FontStyle(bold: true, italic: false, underline: false)
+
+test "parse some rgb color":
+  let style = parseStyle("38;2;123;123;123;1")
+  assert style.get.fg.get.kind == ckRGB
+  assert style.get.fg.get.ckRGBVal == (123u8, 123u8, 123u8)
+  assert style.get.bg.isNone
+  assert style.get.font == FontStyle(bold: true, italic: false, underline: false)
